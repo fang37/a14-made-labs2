0b775f2487502523e6ecde335116d8ba99fbaf1d
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2019-06-18 16:44:42 +0700

    Update Views and Group to Kotlin

---
+++ 01.Views and group/MyViewAndViewGroup/app/src/main/java/com/dicoding/picodiploma/myviewandviews/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.dicoding.picodiploma.myviewandviews
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (supportActionBar != null) {
+            supportActionBar!!.title = "Google Pixel"
+        }
+    }
+}
